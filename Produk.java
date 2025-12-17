71a6f64ed4569a27ad624fb80a98c563a12fa5ab
siyahhh11-ai <siyahhh11@gmail.com>
2025-12-17 07:59:49 +0700

    Update Produk.java

--- Produk.java
+++ Produk.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package pboproject2;
 
@@ -8,11 +8,12 @@ package pboproject2;
  *
  * @author Lenovo
  */
+
 abstract class Produk {
     String nama;
     int harga;
     String size;
-    
+
     // constructor
     public Produk(String size) {
         this.size = size;
@@ -21,7 +22,9 @@ abstract class Produk {
     // abstract method
     public abstract int getHarga();
 
+    // method biasa
     public String getDetail() {
         return nama + " (Size " + size + ") - " + getHarga();
     }
+    
 }
